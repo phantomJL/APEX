f079cc9447be4e13d059e36e8b730546a8f5ee32
lu jiang <jianglu0725@gmail.com>
2020-12-04 17:41:01 -0500

    change margin

--- src/containers/aboutUs/aboutUs.component/service.component.js
+++ src/containers/aboutUs/aboutUs.component/service.component.js
@@ -1,14 +1,14 @@
 import React from "react"
 function Service(props){
     return (
-        <div className = "col-12 col-md-4" >
+        <div className = "col-12 col-md-4 mb-3" >
             <div class="card h-100">
             <div class="card-body h-100">
            <div className="article subtitle card-title py-2">{props.data.title}</div>
            <div className="article paragraph card-text py-2 justify-content-center" >{props.data.intro}</div>
            </div>
            </div>
-           <button type="button" class="btn btn-outline-dark font-weight-bold" >LEARN MORE</button>
+          
         </div>
     )
 }
