28d33174e4a2d93b9f7856c9a1dd3f0b6e0f2206
lu jiang <jianglu0725@gmail.com>
2021-01-11 22:08:44 -0500

    main page

--- src/components/main/main.component.js
+++ src/components/main/main.component.js
@@ -1,38 +1,76 @@
-import React from "react"
-import { withRouter } from "react-router-dom"
-import Nav from "./nav.component"
-import "../../App.css"
-function Main(props){
-    return (
-       <main className="main-canvas">
-           <section className = "sticky-top">
-               <Nav history = {props.history}></Nav>
-           </section>
-           <section className="container-fluid my-auto " style={{ paddingLeft:"60px", paddingRight:"60px"}}>
-               {props.children}
-           </section>
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import Nav from './nav.component';
+import '../../App.css';
 
-           <section class="icon-bar">
-            <a href="https://www.facebook.com/apexcomm" class="facebook">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="https://www.instagram.com/apexcomm/" class="instagram">
-            <i class="fab fa-instagram"></i>
-            </a>
-            <a href="https://www.youtube.com/channel/UCiFfqtera5aCdOApxZzXgxg" class="youtube">
-            <i class="fab fa-youtube-square"></i>
-            </a>
-            <a href="https://vimeo.com/apexcomm" class="vimeo">
-                    <i class="fab fa-vimeo-v"></i>
-                    </a>
-                    <a href="https://www.weibo.com/u/5888176990" class="weibo">
-                    <i class="fab fa-weibo"></i>
-                    </a>
-            </section>
-           <section class="gmail-bar avenir text-uppercase" style={{writingMode: "vertical-rl",textOrientation: "mixed"}}>   hello@apexcomm.us </section>
-           <footer className="trocchi text-weight-bold text-center p-4 text-uppercase small-title" style = {{position:"relative", bottom:"0 !important"}} >© 2020 APEX Communications, INC. All Rights Reserved.</footer>
-       </main>
-    )
+function Main (props) {
+  return (
+    <main
+      className="main-canvas"
+      style={{
+        position: props.history.location.pathname === '/' ? 'relative' : '',
+      }}
+    >
+      <section className="sticky-top">
+        <Nav
+          history={props.history}
+          color={props.history.location.pathname === '/' ? 'black' : 'white'}
+        />
+      </section>
+
+      {props.history.location.pathname === '/'
+        ? <section style={{position: 'absolute', height: '100%'}}>
+            {props.children}
+          </section>
+        : <section>
+            <section
+              className="container-fluid "
+              style={{
+                paddingLeft: '60px',
+                paddingRight: '60px',
+              }}
+            >
+              {props.children}
+            </section> <section class="icon-bar">
+              <a href="https://www.facebook.com/apexcomm" class="facebook">
+                <i class="fab fa-facebook-f" />
+              </a> <a
+                href="https://www.instagram.com/apexcomm/"
+                class="instagram"
+              >
+                <i class="fab fa-instagram" />
+              </a> <a
+                href="https://www.youtube.com/channel/UCiFfqtera5aCdOApxZzXgxg"
+                class="youtube"
+              >
+                <i class="fab fa-youtube-square" />
+              </a> <a href="https://vimeo.com/apexcomm" class="vimeo">
+                <i class="fab fa-vimeo-v" />
+              </a> <a href="https://www.weibo.com/u/5888176990" class="weibo">
+                <i class="fab fa-weibo" />
+              </a>
+            </section> <section
+              class="gmail-bar avenir text-uppercase"
+              style={{
+                writingMode: 'vertical-rl',
+                textOrientation: 'mixed',
+              }}
+            >
+
+              hello @apexcomm.us
+            </section> <footer
+              className="trocchi text-weight-bold text-center p-4 text-uppercase small-title"
+              style={{
+                position: 'relative',
+                bottom: '0 !important',
+              }}
+            >
+              ©2020 APEX Communications, INC.All Rights Reserved.
+            </footer>
+          </section>}
+
+    </main>
+  );
 }
 
-export default withRouter(Main)
+export default withRouter (Main);
--- src/containers/mainPage/mainPage.container.js
+++ src/containers/mainPage/mainPage.container.js
@@ -1,11 +1,20 @@
-import React from "react"
-import { withRouter } from "react-router-dom"
+import React from 'react';
+import {withRouter} from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
-function MainPage(props){
-    return (
-        <main >
-            here is the main page 
-        </main>
-    )
+function MainPage (props) {
+  return (
+    <main style={{height: '100%'}}>
+      <ReactPlayer
+        url={`${process.env.PUBLIC_URL}/video/Reel V5.mp4`}
+        width="100%"
+        height="100%"
+        playing={true}
+        controls={false}
+        loop={true}
+      />
+    </main>
+  );
 }
-export default withRouter(MainPage)
+
+export default withRouter (MainPage);
