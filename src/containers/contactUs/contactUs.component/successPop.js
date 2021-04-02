9f95246b4ad61cf1d9ff3513bdea47c203593f8b
lu jiang <jianglu0725@gmail.com>
2021-04-02 00:45:45 -0400

    change email

--- src/containers/contactUs/contactUs.component/successPop.js
+++ src/containers/contactUs/contactUs.component/successPop.js
@@ -2,13 +2,19 @@ import React from 'react';
 export default function SuccessPop () {
   return (
     <div className="text-center p-4 m-4">
-      <h1 className="p-4 " style={{marginTop: '15%'}}>THANK YOU </h1>
-      <h4 className="p-4 m-4">
-        WE HAVE RECEIEVED YOUR REQUEST AND WILL RESPOND SOON{' '}
+      <img
+        src={`${process.env.PUBLIC_URL}/img/contact/source.gif`}
+        style={{
+          width: '30%',
+          marginTop: '5%',
+        }}
+      />
+      <h1 className="p-4 m-4">THANK YOU !!</h1>
+      <h4 className="p-4 m-4" style={{marginBotton: '15%'}}>
+        YOUR MESSAGE HAS BEEN SENT.
+        WE'LL GET IN TOUCH WITH YOU SHORTLY. {' '}
       </h4>
-      <p className="p-4 " style={{marginBottom: '15%'}}>
-        Please give us a follow to stay up-to-date with the latest news and events
-      </p>
+
     </div>
   );
 }
