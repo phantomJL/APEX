76b4b7f150a94c6bab06f75866327778b114611f
lu jiang <jianglu0725@gmail.com>
2021-07-28 22:15:06 -0400

    change

--- src/containers/mainPage/mainPage.container.js
+++ src/containers/mainPage/mainPage.container.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {withRouter} from 'react-router-dom';
 import ReactPlayer from 'react-player';
 
 function MainPage (props) {
+  const [width, setWidth] = useState (window.innerWidth);
+  const updateDimensions = () => {
+    setWidth (window.innerWidth);
+  };
+  useEffect (
+    () => {
+      window.addEventListener ('resize', updateDimensions);
+      return () => window.removeEventListener ('resize', updateDimensions);
+    },
+    [updateDimensions]
+  );
   return (
     <main style={{height: '100vh'}}>
       <ReactPlayer
         url={`${process.env.PUBLIC_URL}/video/Reel V7.mp4`}
         width="100%"
         height="100%"
-        playing={true}
+        playing={width < 768 ? false : true}
         controls={false}
         muted={true}
         loop={true}
