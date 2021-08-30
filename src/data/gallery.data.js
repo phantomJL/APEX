c218bfdae0f293239faa53a32aef2de14387c16a
lu jiang <jianglu0725@gmail.com>
2021-08-30 18:39:55 -0400

    chang picture

--- src/data/gallery.data.js
+++ src/data/gallery.data.js
@@ -86,6 +86,7 @@ export const video = [
 ];
 
 export const magazine = [
+  {img_url: 'img/gallery/magazine/fala3.png'},
   {img_url: 'img/gallery/magazine/200716x郭采洁0264.jpg'},
   {img_url: 'img/gallery/magazine/200716郭采洁歌力思logo-.jpg'},
   {img_url: 'img/gallery/magazine/200716郭采洁歌力思logo.jpg'},
