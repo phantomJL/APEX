e558635c762d3419c194246074848bc1db040e03
lu jiang <jianglu0725@gmail.com>
2023-01-09 18:04:42 -0500

    change data

--- src/data/caseStudy.data.js
+++ src/data/caseStudy.data.js
@@ -1,7 +1,7 @@
 export const work = [
   {
     case_name: `GLASSMAN × Lay Zhang Cover Story`,
-    location:"November 2022, Los Angeles, CA",
+    location:"November 2022, Los Angeles",
     client:"GLASSMAN China, GLASSMAN UK",
     points: ["Styling coordination",'Budgeting','Pre- + Site-production', "Management of post-production"],
     cover: '/img/caseStudy/work/GlassMan x Lay.JPG',
@@ -10,26 +10,26 @@ export const work = [
     case_name: `The United Nations ActNow Campaign`,
     location:"July 2020, Worldwide",
     collaborator: "The United Nations ",
-    points: ['Talent selection + Communications','Management of content creating '],
+    points: ['Talent selection + Communications','Project management'],
     cover: '/img/caseStudy/work/UN ActNow.JPG',
   },
   {
     case_name: 'Dazed China x Timothée Chalamet Cover Story',
-    location:"February 2020, Los Angeles, CA",
+    location:"February 2020, Los Angeles",
     client:"DAZED China",
     points: ['Casting + Talent communications','Budgeting',"Scheduling + Travel","Pre-production, Site-production, Management of post-production"],
     cover: '/img/caseStudy/work/Timmy x DAZED CN.JPG',
   },
   {
-    case_name: 'Super M’s special live performance on the World Mental Health Day',
-    location:"June 2020, Seoul, South Korea",
+    case_name: 'Super M’s Special Live Performance on the World Mental Health Day',
+    location:"June 2020, Seoul",
     collaborator:"World Health Organization (WHO)",
     points: ['Talent communications','Project management',"Visual direction"],
     cover: '/img/caseStudy/work/SuperM x WHO.jpg',
   },
   {
     case_name: `ELLEMEN Fresh x Jodie Comer Cover Story`,
-    location:"March 2019, Los Angeles, CA",
+    location:"March 2019, Los Angeles",
     client:"ELLEMEN Fresh",
     points: ["Casting + Talent communications",
       "Budgeting",
@@ -40,9 +40,9 @@ export const work = [
   },
   {
     case_name: 'Alyssa Chia at New York Fashion Week (NYFW)',
-    location:"February 2019, New York, NY",
+    location:"February 2019, New York",
     client:"David Yurman",
-    points: ["Budget + Talent fee negotiations",
+    points: ["Budgeting + Talent fee negotiations",
       "Partnership contract",
       "Event appearance",
      "Shoot production + management of live edits and social media posts"
@@ -52,9 +52,9 @@ export const work = [
   },
   {
     case_name: 'Henry Lau at New York Fashion Week (NYFW)',
-    location:"February 2019, New York, NY",
+    location:"February 2019, New York",
     client:"Marc Jacobs",
-    points: ["Talent communications",'Crew sourcing','Site shoot execution', "Partnership deal negotiations and contracting"],
+    points: ["Talent communications",'Crew sourcing','Shoot execution', "Partnership deal negotiations and contracting"],
     cover: '/img/caseStudy/work/Henry Lau x MJ.JPG',
   },
   
@@ -62,21 +62,21 @@ export const work = [
   
   {
     case_name: `Selina Jen at New York Fashion Week (NYFW)`,
-    location:"February 2018, New York, NY",
+    location:"February 2018, New York",
     client:"Michael Kors",
     points: ['Talent communications','Event appearance',"Scheduling","Media relations", "Shoot production"],
     cover: '/img/caseStudy/work/Selina x MK.JPG',
   },
   {
-    case_name: 'Jackson Yee visits the Empire State Building',
-    location:"January 2018, New York, NY",
+    case_name: 'Jackson Yee Visits the Empire State Building',
+    location:"January 2018, New York",
     client:"Jackson Yee",
     points: ['Crew sourcing','Marketing and communications',"Talent communications", "Media relations"],
     cover: '/img/caseStudy/work/Jackson Yee x ESB.JPG',
   },
   {
     case_name: `Alisha Boe at New York Fashion Week (NYFW)`,
-    location:"September 2017, New York, NY",
+    location:"September 2017, New York",
     client:"Vivienne Tam",
     points: ["Talent analysis and selection",
       "Talent fee negotiations",
@@ -86,8 +86,8 @@ export const work = [
     cover: '/img/caseStudy/work/Alisha Boe x VT.JPG',
   },
   {
-    case_name: 'Roy Wang’s speech at the United Nations',
-    location:"January 2017, New York, NY",
+    case_name: 'Roy Wang’s Speech at the United Nations',
+    location:"January 2017, New York",
     client:"Roy Wang",
     points: ['Project management','Talent communications',"Media relations", "Scheduling","Photo and video production"],
     cover: '/img/caseStudy/work/Roy Wang x UN.JPG',
@@ -95,7 +95,7 @@ export const work = [
   
   {
     case_name: `Karry Wang x The Great Wall Press Tour at New York Comic Con`,
-    location:"October 2016, New York, NY",
+    location:"October 2016, New York",
     client:"Karry Wang",
     points: ['Talent communications',"Media relations"],
     cover: '/img/caseStudy/work/Karry Wang x TGW press.JPG',
@@ -103,7 +103,7 @@ export const work = [
   
   {
     case_name: `"Fire Monkey" 2016 Chinese New Year Concert`,
-    location:"February 2016, New York, NY",
+    location:"February 2016, New York",
     points: ['Talent fee negotiations and contracting','Entertainers booking',"Sponsorship negotiations and contracting", "Marketing","Venue + Stage production","Programming","Hospitality","Management of live-streaming","Media relations","Hiring"],
     cover: '/img/caseStudy/work/Fire Monkey.JPG',
   },
